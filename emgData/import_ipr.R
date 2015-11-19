aa7ee8d10e58fc49380f1601167fcb9c1e7ce584
Pier Luigi Buttigieg <p.buttigieg@gmail.com>
2015-11-19 15:57:38 +0100

    Finished import script using mapping file uploaded to ownCloud by @genomewalker at https://owncloud.mpi-bremen.de/index.php/s/eaB3ChiDG6i9C6M/download?path=%2F2014%2Femg&files=OSDlabel-to-ENArun-2015-11-19.tsv

--- emgData/import_ipr.R
+++ emgData/import_ipr.R
@@ -28,4 +28,41 @@ if (exists("rawContextualData") == FALSE){
   source("https://raw.githubusercontent.com/MicroB3-IS/osd-analysis/master/contextualData/import_contextual_data.R")
 } else {print("using existing rawContextualData object")}
 
-sampleMap[, c("osd_id", "ena_acc")]
+enaRun2osdId <- fread(
+  "https://owncloud.mpi-bremen.de/index.php/s/eaB3ChiDG6i9C6M/download?path=%2F2014%2Femg&files=OSDlabel-to-ENArun-2015-11-19.tsv",
+  header = F,
+  sep = "\t",
+  showProgress = F
+  )
+
+setnames(enaRun2osdId, c("osdLabel", "enaRun"))
+setkey(enaRun2osdId, "enaRun")
+
+setnames(
+  iprData,
+  gsub("_MERGED_FASTQ", "", names(iprData))
+)
+
+# extract col names
+iprNames <- data.table(names(iprData))
+setnames(iprNames, "enaRun")
+setkey(iprNames, "enaRun")
+
+# validate...
+#cbind(
+#  merge(iprNames, enaRun2osdId),
+#  enaRun2osdId
+#)
+
+# validate...
+#cbind(
+#  merge(iprNames, enaRun2osdId)[names(iprData), "osdLabel", with = F],
+#  names(iprData)
+#)
+
+
+iprNames.osdLabels <- merge(iprNames, enaRun2osdId)[names(iprData), "osdLabel", with = F]
+iprNames.osdLabels[1] <- "IPR"
+setnames(iprData, iprNames.osdLabels$osdLabel)
+
+
